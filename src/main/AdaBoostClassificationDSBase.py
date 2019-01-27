9173c798e54d3bdface93e4d36719bfee354d63f
Jorge Bernal Chaves <restless_project@hotmail.es>
2019-01-27 19:40:02 +0100

    scaler including in AdaBoosting model

--- src/main/AdaBoostClassificationDSBase.py
+++ src/main/AdaBoostClassificationDSBase.py
@@ -2,6 +2,7 @@ import numpy as np
 import ConstantsDSBase as constants
 
 from sklearn.ensemble import AdaBoostClassifier 
+from sklearn.preprocessing import MinMaxScaler
 from sklearn.externals import joblib
 
 description='AdaBoostClassification'
@@ -15,7 +16,13 @@ class AdaBoostClassificationDSBaseModel:
             print("initiating empty model " + str(self.id) + ". " + description);
             return
 
-        X_train, X_test, y_train, y_test = splitter(X, y, test_size=test_perc, random_state=42)
+        # Normalizing
+        self.scalerX=MinMaxScaler()
+        X_s = self.scalerX.fit_transform(X)
+        self.scalery=MinMaxScaler()
+        y_s = self.scalery.fit_transform(y.reshape(-1, 1))
+
+        X_train, X_test, y_train, y_test = splitter(X_s, y_s, test_size=test_perc, random_state=42)
         self.X_train=X_train
         self.X_test=X_test
         self.y_train=y_train
@@ -30,7 +37,8 @@ class AdaBoostClassificationDSBaseModel:
 
     def predict(self, test_data):
         print("predicting model " + str(self.id) + ". " + description);
-        return self.model.predict(test_data)
+        test_data_norm = self.scalerX.transform(test_data)
+        return self.model.predict(test_data_norm)
         
     def getTrainScore(self):
         return self.model.score(self.X_train, self.y_train);
@@ -42,11 +50,18 @@ class AdaBoostClassificationDSBaseModel:
         file_path=folder_path + constants.SEP + description + "_" + str(self.id) + constants.EXT
         print("saving model: " + file_path)
         joblib.dump(self.model, file_path)
+        scaler_file_path_root=folder_path + constants.SEP + description + "_" + str(self.id)
+        joblib.dump(self.scalerX, scaler_file_path_root + "_scalerX" + constants.EXT)
+        joblib.dump(self.scalery, scaler_file_path_root + "_scalery" + constants.EXT)
     
     def load(self, folder_path=constants.PERSISTANCE_FOLDER):
         file_path=folder_path + constants.SEP + description + "_" + str(self.id) + constants.EXT
         print("loading model: " + file_path)
+        scaler_file_path_root=folder_path + constants.SEP + description + "_" + str(self.id)
         self.model = joblib.load(file_path)
+        self.scalerX=joblib.load(scaler_file_path_root + "_scalerX" + constants.EXT)
+        self.scalery=joblib.load(scaler_file_path_root + "_scalery" + constants.EXT)
+
 
     def close(self):
         pass
