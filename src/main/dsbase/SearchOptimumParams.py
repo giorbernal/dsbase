0d1f948f3a4e235b399ba1ac6133151598ffbe3c
Jorge Bernal Chaves <restless_project@hotmail.es>
2019-09-14 23:26:47 +0200

    bug fixing

--- src/main/dsbase/SearchOptimumParams.py
+++ src/main/dsbase/SearchOptimumParams.py
@@ -7,7 +7,7 @@ from dsbase.KFoldDSBase import KFoldDSBase
 def evaluateParams(X, y, k_fold, model_class, model_prefix_name, params_list, num_tries):
 	tries = {}
 	for i in range(num_tries):
-	    kf = KFoldDSBase(X, y, 5, model_class, model_prefix_name, params_list[i])
+	    kf = KFoldDSBase(X, y, k_fold, model_class, model_prefix_name, params_list[i])
 	    kf.train()
 	    score_train, score_test = kf.getMeanScore()
 	    print('****** Result try',i,':',score_train,' / ',score_test)
